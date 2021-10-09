bd7af26a43734ab65e78e64183cc14568f1424a3
Jeramy Sena <jeramy_sena@hotmail.com>
2021-10-09 17:31:23 -0500

    saving progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,52 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+
+// Each value available for the password is listed out in their own array.
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o",  "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numberValue = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specialCharacters = ["@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+var selectedArray = [];
+var userArray = [];
+
+// Window prompt for each array/value for password.
+  var numCharacter = window.prompt ("How many characters do you want between 8 and 128?");
+  var numbers = window.confirm ("Do you want numbers in your password?");
+  var uppercases = window.confirm ("Do you want uppercases letters in your password?");
+  var lowercases = window.confirm ("Do you want lowercases letters in your password?");
+  var characters = window.confirm ("Do you want special characters in your password?");
+
+  
+
+// condition of the array
+
+if (numbers){
+  selectedArray = selectedArray.concat(numberValue);
+}
+
+if (uppercases){
+  selectedArray = selectedArray.concat(upperCase);
+}
+
+if (lowercases){
+  selectedArray = selectedArray.concat(lowerCase);
+}
+
+if (characters){
+  selectedArray = selectedArray.concat(specialCharacters);
+}
+console.log(selectedArray)
+
+
+for (var i = 0; i < numCharacter; i++) {
+  userArray.push (selectedArray[Math.floor(Math.random() * selectedArray.length)]); 
+  }
+  return userArray.join("") ;
+}
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,5 +56,12 @@ function writePassword() {
 
 }
 
+function copyToClipboard() {
+  // BONUS 
+}
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// BONUS EVENT LISTENER
